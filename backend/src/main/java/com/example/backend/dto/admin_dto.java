17596e05238b4cc7543202d4bd16cd9a8d6e1887
pruthvi4everyone <pruthvireddy100@gmail.com>
2025-06-17 20:30:52 -0400

    [backend]APIs created

---
+++ backend/src/main/java/com/example/backend/dto/admin_dto.java
@@ -0,0 +1,20 @@
+package com.example.backend.dto;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class admin_dto {
+
+    private String name;
+
+    private String email;
+
+    private String password;
+}
