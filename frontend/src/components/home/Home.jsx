83224a320166f248b4d500dc62824d52014790ec
rachithachalla <51987147+rachithachalla@users.noreply.github.com>
2025-06-19 22:44:53 -0400

    Update Home.jsx

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -1,14 +1,7 @@
 import React, { useEffect } from "react";
 import "./Home.css";
-import { useAuth } from "../../hooks/useAuth";
 
 function Home() {
-  const {logout} = useAuth();
-  /* ------------------- logout when visited this page ------------------- */
-  useEffect(() => {
-    logout(); // auto-logout on visiting this page
-  }, []);
-  /* --------------------------------------------------------------------- */
   
   return (
     <div className="home-container">
